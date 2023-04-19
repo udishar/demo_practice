7ab57a9cea948d89a0d3589a11343128b8f85647
udishar <udisha.arrawatia@gmail.com>
2023-04-19 11:49:29 +0530

    files added

---
+++ file.js
@@ -0,0 +1,13 @@
+"use strict";
+// let a = 10
+// console.log(a)
+// export {}
+// class demo{
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 10;
+console.log(a);
+var b = "udisha";
+console.log(b);
+//  ERROR :=> string is not assignable to number
+// let c :number = "ankush"
+// console.log(c)
---
+++ file.ts
@@ -0,0 +1,25 @@
+
+// let a = 10
+// console.log(a)
+// export {}
+// class demo{
+
+//     test(){
+//         console.log("hello this is test function")
+//     }
+// }
+
+// let demo1 = new demo()
+// demo1.test()
+
+// types in typescript
+export{}
+let a:number=10;
+console.log(a)
+
+let b :string="udisha"
+console.log(b)
+
+//  ERROR :=> string is not assignable to number
+// let c :number = "ankush"
+// console.log(c)
